e6daa671b2edd86d88b6f54f1005734e37331d05
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-28 21:10:38 +0300

    Updates

--- precache-manifest.a6942b272ffa1a88f6c93d84ad129ac0.js
+++ precache-manifest.a6942b272ffa1a88f6c93d84ad129ac0.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "66f3db107f378b2cbc0323e7a209143e",
+    "revision": "6e5b9bea49257259be4df4e1fe397cc2",
     "url": "/iraq-bc-movies-project-students/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/iraq-bc-movies-project-students/static/css/2.0e1a4c73.chunk.css"
   },
   {
-    "revision": "36231a64997f271ed2c9",
-    "url": "/iraq-bc-movies-project-students/static/css/main.f0a60877.chunk.css"
+    "revision": "83e22df403200189fa9f",
+    "url": "/iraq-bc-movies-project-students/static/css/main.00b370de.chunk.css"
   },
   {
     "revision": "47b51e1a1cd5dcc75ee7",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/iraq-bc-movies-project-students/static/js/2.b7add91d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "36231a64997f271ed2c9",
-    "url": "/iraq-bc-movies-project-students/static/js/main.a0013511.chunk.js"
+    "revision": "83e22df403200189fa9f",
+    "url": "/iraq-bc-movies-project-students/static/js/main.2107a418.chunk.js"
   },
   {
     "revision": "8b2f1f5418ec522d8af9",
